bc6de768c0a1f28f07485cf2f572b940092c2e1c
sajoesor <159174441+sajoesor@users.noreply.github.com>
2025-05-10 16:19:45 -0500

    api.js

--- api.js
+++ api.js
@@ -1,10 +1,10 @@
 export async function fetchSalones() {
-  const res = await fetch("http://localhost:5000/api/salones")
+  const res = await fetch("https://javeriana-back.vercel.app/api/salones")
   return res.json()
 }
 
 export async function preguntarChatGPT(pregunta) {
-  const res = await fetch("http://localhost:5000/api/salones/preguntar", {
+  const res = await fetch("https://javeriana-back.vercel.app/api/salones/preguntar", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ pregunta }),
